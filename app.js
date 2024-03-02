35ad0faf2a8282e83b0717feea1f4cc8ab21bc1e
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-03-02 09:21:06 +0500

    added all

---
+++ app.js
@@ -0,0 +1,159 @@
+// chapter-4
+// // Q1
+// var firstName =("Rule")
+// var a= ("of")
+// var b= ("naming")
+// var lastName =("variables")
+// document.write ("<h3>"+ firstName+"\n" + a+"\n" + b+"\n" + lastName +"</h3>")
+
+
+
+// Q2
+// legel-name
+
+
+// var sufiyan=("sufiyan")
+// var pk1=("pk1")
+// var pak12ssa=("pak12ssa")
+// var $dollar=("$dollar")
+// var firstName=("firstname")
+// document.write("<h2>legal-name</h2>"+ sufiyan+"\n"+pk1+"\n"+pak12ssa+"\n"+"\n"+$dollar+"\n"+"\n"+firstName)
+
+
+// Q2
+// ilegel-name
+
+
+// var a=("1name")
+// var b=("sk-sa")
+// var c=("var")
+// var d=("alert")
+// var e=("document")
+
+// document.write("<h2>ilegal-name</h2>"+ a+"\n"+b+"\n"+c+"\n"+d+"\n"+e)
+
+// Q3
+// A
+
+// var myvar=("myvar")
+// var user_document=("user_document")
+// var persnal_alert=("persnal_alert")
+// document.write("<h3>"+ myvar+"\n"+"\n"+user_document+"\n"+"\n"+persnal_alert+"</h3>")
+
+// Q3
+// B
+
+// var $my_logername=("$my_logername")
+// var feature12name=("feature12name")
+// var variables=("variables")
+// var text2=("text2")
+// document.write($my_logername+"\n"+"\n"+feature12name+"\n"+"\n"+variables+"\n"+"\n"+text2)
+
+// Q3
+// c
+//  var $auto=("$auto")
+//  var action12name=(" action12name")
+//  var anker=("anker")
+
+//  document.write($auto+"\n"+"\n"+ action12name+"\n"+"\n"+anker)
+
+// Q3
+// d
+// document.write("d) Variable names are case <br> 'sensitive'.")
+
+// Q3
+// e
+// document.write("e) Variable names should not be JS  <br> 'keywords'.")
+
+// chapter-5
+// Q1
+// var a=(3);
+// var b=(5);
+// var c=(8);
+// document.write(a+b+c)
+
+// Q2
+// a
+// multiply
+// var a=(3);
+// var b=(5);
+// var c=(8);
+// document.write(a*b*c)
+
+// Q2
+// b
+// dived
+// var a=(3);
+// var b=(5);
+// var c=(8);
+// document.write(a/b/c)
+// Q2
+// c
+
+// var a=(3);
+// var b=(5);
+// var c=(8);
+// document.write(a-b-c)
+
+// Q3
+// a
+// var x =2;
+// var y=4;
+// var z=x+y;
+// document.write(z)
+
+// b
+// var c=(9);
+// var d=(2);
+// var e=(1);
+// document.write(c-d-+e)]
+
+
+
+// Q3
+// 
+
+
+// Q4
+// var price=(600);
+// document.write("<h1>"+ "Total cost to Buy 5 tickits to a movie is "+ price*5+("PKR")+"</h1>")
+
+// Q5
+// document.write(
+//   "<h4>" + "Table \n of \n 4"+"</h4>"
+// )
+// var a=("4 \n x")
+// var b=("\n1 \n = \n 4")
+// document.write(a+b)
+// b=("\n2 \n = \n 8")
+// document.write("<br>"+ a+b)
+// b=("\n3 \n = \n 12")
+// document.write("<br>"+ a+b)
+// b=("\n4 \n = \n 16")
+// document.write("<br>"+ a+b)
+// b=("\n5 \n = \n 20")
+// document.write("<br>"+ a+b)
+// b=("\n6 \n = \n 24")
+// document.write("<br>"+ a+b)
+// b=("\n7 \n = \n 28")
+// document.write("<br>"+ a+b)
+// b=("\n8 \n = \n 32")
+// document.write("<br>"+ a+b)
+// b=("\n9 \n = \n 36")
+// document.write("<br>"+ a+b)
+// b=("\n10 \n = \n 40")
+// document.write("<br>"+ a+b)
+
+// Q7
+// var price1OfItem= (650)
+// var price2OfItem= (100)
+// var quantiiy1is=(3)
+// var quantiiy2is=(2)
+// var shipping_charge=(100)
+// document.write("<h1>Shopping Card</h1>")
+// document.write("Price Of 1 Items is :\n"+price1OfItem +"<br>")
+// document.write("Quantity Of 1 Items is:\n"+quantiiy1is +"<br>")
+// document.write(" Price Of 2 Items is:\n"+price2OfItem+"<br>")
+// document.write("Quantity Of 2 Items is:\n "+quantiiy2is +"<br>")
+// document.write("Shipping Charge:\n "+shipping_charge +"<br>")
+// document.write("Total Amount Items:\n "+((650*3)+(100*2)+100))
